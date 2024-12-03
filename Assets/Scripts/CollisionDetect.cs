d750c4684ac8f326f79c2e5d6c11fa7960b51b54
Remilekun-Odegbami <remilekunodegbami@gmail.com>
2024-12-03 23:35:43 +0100

    worked on player collision with obstacles

---
+++ Assets/Scripts/CollisionDetect.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+    [SerializeField] AudioSource collideFX;
+    [SerializeField] GameObject thePlayer;
+    [SerializeField] GameObject playerAnimation;
+    private void OnTriggerEnter(Collider other)
+    {
+        collideFX.Play();
+
+        thePlayer.GetComponent<Player>().enabled = false;
+        playerAnimation.GetComponent<Animator>().Play("Stumble Backwards");
+    }
+}

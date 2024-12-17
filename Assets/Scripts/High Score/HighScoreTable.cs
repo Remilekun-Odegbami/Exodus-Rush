4d2d8b88aab1d2a6da585de925fa801b00334084
Remilekun-Odegbami <remilekunodegbami@gmail.com>
2024-12-17 07:34:51 +0100

    working on highscore table

--- Assets/Scripts/High Score/HighScoreTable.cs
+++ Assets/Scripts/High Score/HighScoreTable.cs
@@ -1,33 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighScoreTable : MonoBehaviour
 {
-    private Transform entryContainer;
-    private Transform entryTemplate;
-    [SerializeField] public GameObject entryContainer2;
-    [SerializeField] public GameObject entryTemplate2;
+    [SerializeField] private GameObject entryContainer;
+    [SerializeField] private GameObject entryTemplate;
     private void Awake()
     {
-        entryContainer = transform.Find("HighscoreEntryContainer");
-        entryTemplate = transform.Find("HighscoreEntryTemplate");
-        if (entryTemplate == null)
-        {
-            Debug.LogError("Could not find HighscoreEntryContainer object.");
-            return;
-        } else if(entryContainer != null)
-        {
-            Debug.Log("Found");
-        }
+        entryTemplate.gameObject.SetActive(false);
 
-
-        entryTemplate2.gameObject.SetActive(false);
-        float templateHeight = 20f;
+        float templateHeight = 55f;
         for (int i = 0; i < 10; i++)
         {
-            // Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            GameObject entryGameObject = Instantiate(entryTemplate2, entryContainer2.transform);
+            GameObject entryGameObject = Instantiate(entryTemplate, entryContainer.transform);
             Transform entryTransform = entryGameObject.transform;
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
